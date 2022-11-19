38e80f4439f94796c5c8d33b8e82ff4624936a30
Gats237 <henning.silas.pils@hs-furtwangen.de>
2022-11-19 21:38:40 +0100

    Push

---
+++ A06_DataClient/A06_Interface copy.ts
@@ -0,0 +1,30 @@
+/*
+Aufgabe: Aufgabe 3 Einkaufsliste
+Name: Henning Pils
+Matrikel: 269355
+Datum: 27.10.2022
+Quellen: Robert Schindler, Yannik König!!, Jonas Atzenhofer, 
+*/
+
+namespace A05_Einkaufsliste_Client{ 
+ 
+ //console.log("start");
+
+export interface ItemAdded {
+        Eintragen:string;
+        Menge: number;
+        Notiz: string;
+        checkbox1: boolean;
+        date: string;
+    };
+
+export interface Data { 
+    [itemNumber: number]: ItemAdded[];
+}
+
+export let data: Data = {
+    1:[
+        {Eintragen: "Brot", Menge:1, Notiz: "Bauerbrot", checkbox1: false, date: "27.10.22"}
+    ],
+};
+}
