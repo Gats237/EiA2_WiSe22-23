be1284b23662eebdbf4c72ba569436bbbacbf7bd
Gats237 <henning.silas.pils@hs-furtwangen.de>
2023-01-16 14:18:36 +0100

    Save

--- Endabe2/sript.js
+++ Endabe2/sript.js
@@ -4,12 +4,13 @@ var Auswahl;
     console.log("Verklinkung funktioniert");
     window.addEventListener("load", handleLoad);
     let Questions = [];
+    //Counter Richtige Fragen/ Falsche Fragen/ Punkte   / Fragen
     let pointcounter = 0;
-    let questioncounter = 0;
     let wrongquestioncounter = 0;
+    let questioncounter = 0;
     let endGame = false;
     //Template für Fragen
-    //{question: "", answers: ["", "","",""], correctAnswer: "", link: ""},
+    //{question: "", answers: ["", "","",""], correctAnswer: ""}
     //Fragen für Themen Arrays
     let HMTLQuestion = [
         { question: "Ist HTML eine Programmiersprache?", answers: ["Ja", "Nein", "vielleicht", "Nur Montags"], correctAnswer: "Nein", link: "" },
@@ -17,9 +18,8 @@ var Auswahl;
         { question: "Welche Tags müssen nicht geschlossen werden?", answers: ["void", "p", "h", "div"], correctAnswer: "void", link: "https://felix.hs-furtwangen.de/m/cd997f63ae79858c56cdd00e2f3e7556/L02_Einstieg_HTML.pdf" },
         { question: "Welches Metachrt wird üblich verwendet", answers: ["USA-5", "UTF-8", "WWW", "UTF-3"], correctAnswer: "UTF-8", link: "" },
         { question: "Aus was bestehen die meisten HTML Tags", answers: ["Einen öffenden Tag und einen schließenden Tag", "Einen Selektor", "Einen Link", "Haben keine Gemeinsamkeiten"], correctAnswer: "Einen öffenden Tag und einen schließenden Tag", link: "" },
-        { question: "Wie sollten CSS Elemente am besten eingebungen werden ", answers: ["inline", "", "", ""], correctAnswer: "", link: "" },
         { question: "Wie wirde das Wort Semantik noch genannt", answers: ["Praxis", "Zeichen", "Theorie", "Bedeutungslehre"], correctAnswer: "Bedeutungslehre", link: "https://felix.hs-furtwangen.de/m/e4d06cfb1be786728869f70d91dfa7e2/L03_HTML5_Familie.pdf" },
-        { question: "Was Element ist bei einer HTML Element an ertser Stelle", answers: ["<footer>", "<header>", "<main>", "<head>"], correctAnswer: "<head>", link: "https://felix.hs-furtwangen.de/m/e4d06cfb1be786728869f70d91dfa7e2/L03_HTML5_Familie.pdf" },
+        { question: "Was ist bei einer HTML Element an ertser Stelle", answers: ["footer", "header", "main", "head"], correctAnswer: "head", link: "https://felix.hs-furtwangen.de/m/e4d06cfb1be786728869f70d91dfa7e2/L03_HTML5_Familie.pdf" },
         { question: "Welche Aufgabe hat der Header in der Regel?", answers: ["Informationen: z.B. Hauptnavigation, Brand (typischerweise Logo), Metanavigationen (Sprachswitch, ...) ", "Primär / Sekundärnavigation", "Typische Seiteninformationen / Marginalspalten mit ergänzenden Zusatzinformationen zum eigentlichen Inhalt", "Kontakt, Impressum (seit DSGVO). Auch: Fußnoten im wissenscha!lichen Sinne"], correctAnswer: "Informationen: z.B. Hauptnavigation, Brand (typischerweise Logo), Metanavigationen (Sprachswitch, ...) ", link: "https://felix.hs-furtwangen.de/m/e4d06cfb1be786728869f70d91dfa7e2/L03_HTML5_Familie.pdf" },
         { question: "Was heißt DOM", answers: ["Document Object Model", " Document Oriented Markup ", "Document Over Mainsystem", "Document Object Markup "], correctAnswer: "Document Object Model", link: "https://felix.hs-furtwangen.de/m/cd997f63ae79858c56cdd00e2f3e7556/L02_Einstieg_HTML.pdf" },
         { question: "Was ist kein Block Elememt", answers: ["h1", "p", "div", "img"], correctAnswer: "img", link: "https://felix.hs-furtwangen.de/m/cd997f63ae79858c56cdd00e2f3e7556/L02_Einstieg_HTML.pdf" },
@@ -35,7 +35,7 @@ var Auswahl;
     let Gemischt = [];
     // //Buttons Auswahl Themen zuweisen 
     function handleLoad() {
-        console.log("lll");
+        console.log("Themen");
         let T1 = document.getElementById("T1");
         let T2 = document.getElementById("T2");
         let T3 = document.getElementById("T3");
@@ -69,6 +69,11 @@ var Auswahl;
         //Clear Inner HTML für alle Buttons und die Frage
         let wrong2 = document.getElementById("falsch");
         wrong2.style.display = "none";
+        //Punktestand Konsolen überprüfung
+        console.log("Falsche Antowrten", wrongquestioncounter);
+        console.log("Anzahl der Fragen", questioncounter);
+        console.log("Richtige Fragen", pointcounter);
+        //endGame();
         console.log("update");
         let Question;
         Question = Questions[0];
@@ -114,18 +119,12 @@ var Auswahl;
         }
         function checkAnswer(answer, correctAnswer) {
             if (compareAnswers(answer, correctAnswer) == true) {
-                console.log("Richtig1");
-                pointcounter++;
-                questioncounter++;
-                ;
+                console.log("Richtig");
                 rightAnswers();
             }
             else {
                 console.log("Falsch");
                 wrongAnswers();
-                wrongquestioncounter++;
-                questioncounter++;
-                console.log(questioncounter);
             }
         }
         //Richtige Antowrten werden angezeigt
@@ -134,15 +133,19 @@ var Auswahl;
             right.style.display = "block";
             let wrong = document.getElementById("falsch");
             wrong.style.display = "none";
-            console.log("Haool");
+            console.log("Richtig Antwort");
+            pointcounter++;
+            questioncounter++;
         }
         //Falsche Antworten werden angezeigt
         function wrongAnswers() {
-            console.log("Falsch");
+            console.log("Falsch Antwort");
             let wrong = document.getElementById("falsch");
             wrong.style.display = "block";
             let right2 = document.getElementById("richtig");
             right2.style.display = "none";
+            wrongquestioncounter++;
+            questioncounter++;
         }
         //nächste Frage Eventlistener für den Button/ richtig und falsche Antworten werden ausgeblendet beim klick auf den Button
         let nextQ = document.getElementById("nextQ");
@@ -152,63 +155,59 @@ var Auswahl;
             let right2 = document.getElementById("richtig");
             right2.style.display = "none";
         });
-    }
-    //Punkte werden angezeigt
-    function showPoi() {
-        let points = document.getElementById("counter-value");
-        points.innerHTML = pointcounter.toString();
-        let wrongpoints = document.getElementById("wrongcounter-value");
-        wrongpoints.innerHTML = wrongquestioncounter.toString();
-    }
-    //Spiel wird beendet
-    function goalCounter() {
-        if (pointcounter == 5) {
-            endGame();
+        //Punkte werden angezeigt
+        function showPoi() {
+            let points = document.getElementById("counter-value");
+            points.innerHTML = pointcounter.toString();
+            let wrongpoints = document.getElementById("wrongcounter-value");
+            wrongpoints.innerHTML = wrongquestioncounter.toString();
         }
     }
-    //function endGame(): void { 
-    let end = document.getElementById("End");
-    end.style.display = "block";
-    let hidden = document.getElementById("question-container");
-    hidden.style.display = "none";
-    let hidden2 = document.getElementById("End");
-    hidden2.style.display = "block";
-    let hidden3 = document.getElementById("ResultR");
-    hidden3.style.display = "none";
-    let hidden4 = document.getElementById("wrong");
-    hidden4.style.display = "none";
-    let hidden5 = document.getElementById("Themengebiet");
-    hidden5.style.display = "block";
-    let hidden6 = document.getElementById("question");
-    hidden6.style.display = "block";
-    let hidden7 = document.getElementById("A");
-    hidden7.style.display = "block";
-    let hidden8 = document.getElementById("B");
-    hidden8.style.display = "block";
-    let hidden9 = document.getElementById("C");
-    hidden9.style.display = "block";
-    let hidden10 = document.getElementById("D");
-    hidden10.style.display = "block";
-    let hidden11 = document.getElementById("End");
-    hidden11.style.display = "block";
-    let hidden12 = document.getElementById("ResultR");
-    hidden12.style.display = "block";
-    let hidden13 = document.getElementById("wrong");
-    hidden13.style.display = "block";
-    let hidden14 = document.getElementById("End");
-    hidden14.style.display = "block";
-    let hidden15 = document.getElementById("ResultR");
-    hidden15.style.display = "block";
-    let hidden16 = document.getElementById("wrong");
-    hidden16.style.display = "block";
-    let hidden17 = document.getElementById("End");
-    hidden17.style.display = "block";
-    let hidden18 = document.getElementById("ResultR");
-    hidden18.style.display = "block";
-    let hidden19 = document.getElementById("wrong");
-    hidden19.style.display = "block";
-    let hidden20 = document.getElementById("End");
 })(Auswahl || (Auswahl = {}));
+//Spiel wird beendet
+//function endGame(): void { 
+// if (pointcounter == 5) {
+// let end: HTMLElement = document.getElementById("End") as HTMLElement;
+// end.style.display = "block";
+// let hidden: HTMLElement = document.getElementById("question-container") as HTMLElement;
+// hidden.style.display = "none";
+// let hidden2: HTMLElement = document.getElementById("End") as HTMLElement;
+// hidden2.style.display = "block";
+// let hidden3: HTMLElement = document.getElementById("ResultR") as HTMLElement;
+// hidden3.style.display = "none";
+// let hidden4: HTMLElement = document.getElementById("wrong") as HTMLElement;
+// hidden4.style.display = "none";
+// let hidden5: HTMLElement = document.getElementById("Themengebiet") as HTMLElement;
+// hidden5.style.display = "block";
+// let hidden6: HTMLElement = document.getElementById("question") as HTMLElement;
+// hidden6.style.display = "block";
+// let hidden7: HTMLElement = document.getElementById("A") as HTMLElement;
+// hidden7.style.display = "block";
+// let hidden8: HTMLElement = document.getElementById("B") as HTMLElement;
+// hidden8.style.display = "block";
+// let hidden9: HTMLElement = document.getElementById("C") as HTMLElement;
+// hidden9.style.display = "block";
+// let hidden10: HTMLElement = document.getElementById("D") as HTMLElement;
+// hidden10.style.display = "block";
+// let hidden11: HTMLElement = document.getElementById("End") as HTMLElement;
+// hidden11.style.display = "block";
+// let hidden12: HTMLElement = document.getElementById("ResultR") as HTMLElement;
+// hidden12.style.display = "block";
+// let hidden13: HTMLElement = document.getElementById("wrong") as HTMLElement;
+// hidden13.style.display = "block";
+// let hidden14: HTMLElement = document.getElementById("End") as HTMLElement;
+// hidden14.style.display = "block";
+// let hidden15: HTMLElement = document.getElementById("ResultR") as HTMLElement;
+// hidden15.style.display = "block";
+// let hidden16: HTMLElement = document.getElementById("wrong") as HTMLElement;
+// hidden16.style.display = "block";
+// let hidden17: HTMLElement = document.getElementById("End") as HTMLElement;
+// hidden17.style.display = "block";
+// let hidden18: HTMLElement = document.getElementById("ResultR") as HTMLElement;
+// hidden18.style.display = "block";
+// let hidden19: HTMLElement = document.getElementById("wrong") as HTMLElement;
+// hidden19.style.display = "block";
+// let hidden20: HTMLElement = document.getElementById("End") as HTMLElement;
 //Für die Antworten nochmal ein shuffle, und eine for schleife, die auf die Buttons zugreift
 // const questionContainer  = document.getElementById("questionContainer");
 // for (let i = 0; i < questionHTML.length; i++) {
--- Endabe2/sript.ts
+++ Endabe2/sript.ts
@@ -5,16 +5,23 @@ console.log("Verklinkung funktioniert");
 window.addEventListener("load", handleLoad);
 
 //Globale Variablen
-interface Question { question: string; answers: string[]; correctAnswer: string; link: string;}
+interface Question { question: string; answers: string[]; correctAnswer: string; }
 let Questions :Question [] = [ ];
+
+
+
+//Counter Richtige Fragen/ Falsche Fragen/ Punkte   / Fragen
 let pointcounter: number = 0;
-let questioncounter: number = 0;
 let wrongquestioncounter: number = 0;
+let questioncounter: number = 0;
+
+
+
 
 let endGame: boolean = false;
 
 //Template für Fragen
- //{question: "", answers: ["", "","",""], correctAnswer: "", link: ""},
+ //{question: "", answers: ["", "","",""], correctAnswer: ""}
 
 
 //Fragen für Themen Arrays
@@ -24,13 +31,11 @@ let HMTLQuestion: Question [] = [
     {question: "Welche Tags müssen nicht geschlossen werden?", answers: ["void", "p","h","div"], correctAnswer: "void", link: "https://felix.hs-furtwangen.de/m/cd997f63ae79858c56cdd00e2f3e7556/L02_Einstieg_HTML.pdf"},
     {question: "Welches Metachrt wird üblich verwendet", answers: ["USA-5", "UTF-8","WWW","UTF-3"], correctAnswer: "UTF-8",link: ""},
     {question: "Aus was bestehen die meisten HTML Tags", answers: ["Einen öffenden Tag und einen schließenden Tag", "Einen Selektor","Einen Link","Haben keine Gemeinsamkeiten"], correctAnswer: "Einen öffenden Tag und einen schließenden Tag",link: ""},
-    {question: "Wie sollten CSS Elemente am besten eingebungen werden ", answers: ["inline", "","",""], correctAnswer: "",link: ""},
     {question: "Wie wirde das Wort Semantik noch genannt", answers: ["Praxis", "Zeichen","Theorie","Bedeutungslehre"], correctAnswer: "Bedeutungslehre",link: "https://felix.hs-furtwangen.de/m/e4d06cfb1be786728869f70d91dfa7e2/L03_HTML5_Familie.pdf"},
-    {question: "Was Element ist bei einer HTML Element an ertser Stelle", answers: ["<footer>", "<header>","<main>","<head>"], correctAnswer: "<head>", link: "https://felix.hs-furtwangen.de/m/e4d06cfb1be786728869f70d91dfa7e2/L03_HTML5_Familie.pdf"},
+    {question: "Was ist bei einer HTML Element an ertser Stelle", answers: ["footer", "header","main","head"], correctAnswer: "head", link: "https://felix.hs-furtwangen.de/m/e4d06cfb1be786728869f70d91dfa7e2/L03_HTML5_Familie.pdf"},
     {question: "Welche Aufgabe hat der Header in der Regel?", answers: ["Informationen: z.B. Hauptnavigation, Brand (typischerweise Logo), Metanavigationen (Sprachswitch, ...) ", "Primär / Sekundärnavigation","Typische Seiteninformationen / Marginalspalten mit ergänzenden Zusatzinformationen zum eigentlichen Inhalt","Kontakt, Impressum (seit DSGVO). Auch: Fußnoten im wissenscha!lichen Sinne"], correctAnswer: "Informationen: z.B. Hauptnavigation, Brand (typischerweise Logo), Metanavigationen (Sprachswitch, ...) ", link: "https://felix.hs-furtwangen.de/m/e4d06cfb1be786728869f70d91dfa7e2/L03_HTML5_Familie.pdf"},
     {question: "Was heißt DOM", answers: ["Document Object Model", " Document Oriented Markup ","Document Over Mainsystem","Document Object Markup "], correctAnswer: "Document Object Model", link: "https://felix.hs-furtwangen.de/m/cd997f63ae79858c56cdd00e2f3e7556/L02_Einstieg_HTML.pdf"},
     {question: "Was ist kein Block Elememt", answers: ["h1", "p","div","img"], correctAnswer: "img", link: "https://felix.hs-furtwangen.de/m/cd997f63ae79858c56cdd00e2f3e7556/L02_Einstieg_HTML.pdf"},
-
 ];
 
 let CSSQuestion: Question [] = [
@@ -39,6 +44,7 @@ let CSSQuestion: Question [] = [
     {question: "Welche Methode für die Darstellung mit CSS von Web-Anwendungen  wirdempfohlen?", answers: ["inline", "im Head","als externes Dokument","outline"], correctAnswer: "als externes Dokument", link: "https://felix.hs-furtwangen.de/m/a08c56bdd4ce8b0a1733dbfa6d82bf3c/L04-1_CSS_I_Einfuehrung.pdf"},
     {question: "Mit welchem Zeichen wird eine ID mit CSS selektiert?", answers: [".", "#","()","/"], correctAnswer: "#", link: "https://felix.hs-furtwangen.de/m/a08c56bdd4ce8b0a1733dbfa6d82bf3c/L04-2_CSS_I-Selektoren_v2.pdf"},
     {question: "Mit welchem Zeichen wird eine class mit CSS selektiert?", answers: [".", "#","()","/"], correctAnswer: ".", link: "https://felix.hs-furtwangen.de/m/a08c56bdd4ce8b0a1733dbfa6d82bf3c/L04-2_CSS_I-Selektoren_v2.pdf"},
+   
 ];
 
 
@@ -48,7 +54,7 @@ let Gemischt: Question [] = [];
 
 // //Buttons Auswahl Themen zuweisen 
 function handleLoad(): void {
-    console.log("lll")
+    console.log("Themen")
     let T1 = document.getElementById("T1") as HTMLButtonElement;
     let T2 = document.getElementById("T2") as HTMLButtonElement;
     let T3 = document.getElementById("T3") as HTMLButtonElement;
@@ -58,7 +64,7 @@ function handleLoad(): void {
     T2.addEventListener("click", function(){navigateToGame(CSSQuestion)});
     T3.addEventListener("click", function(){navigateToGame(TSQuestion)});
     T4.addEventListener("click", function(){navigateToGame(Gemischt)});
-    
+
 
 }
 
@@ -87,9 +93,6 @@ function navigateToGame(_Thema:Question []): void {
     hidden2.style.display = "none";
 
     update();
-    
-
-
     console.log("HALLO");
 }
 
@@ -101,12 +104,19 @@ function update (): void {
     //Clear Inner HTML für alle Buttons und die Frage
     let wrong2: HTMLElement = document.getElementById("falsch") as HTMLElement;
     wrong2.style.display = "none";
+    
 
-console.log("update");
+    //Punktestand Konsolen überprüfung
+    console.log("Falsche Antowrten", wrongquestioncounter);
+    console.log("Anzahl der Fragen", questioncounter);
+    console.log("Richtige Fragen", pointcounter);
+    //endGame();
+    
+
+    console.log("update");
     let Question :Question;
     Question = Questions[0]
     Questions.splice(0, 1)
-    
 
     for (let i = 0; i < 8; i++) {
         //gibt eine zufällige Zahl zwischen 0 und 3 aus aus dem Array
@@ -119,8 +129,6 @@ console.log("update");
         Question.answers[loc1] = Question.answers[loc2];
         Question.answers[loc2] = tmp;
     }
-
-
 
 
         //DOM Manipulation mit den INformationen aus Question
@@ -154,25 +162,26 @@ console.log("update");
         function compareAnswers(answer: string, correctAnswer: string): boolean {
             if (answer == correctAnswer) {
                 return true;
+               
+                
             } else {
                 return false;
+            
             }
         }
         
         function checkAnswer(answer: string, correctAnswer: string): void { 
             if (compareAnswers(answer, correctAnswer) == true) {
-                console.log("Richtig1");
-                pointcounter++;
-                questioncounter++;
-        ;
+                console.log("Richtig");
                 rightAnswers();
-
+              
+                
             } else {
                 console.log("Falsch");
                 wrongAnswers();
-                wrongquestioncounter++;
-                questioncounter++;
-                console.log(questioncounter);
+              
+               
+                
                 
                 
 
@@ -187,7 +196,9 @@ console.log("update");
         right.style.display = "block";
         let wrong: HTMLElement = document.getElementById("falsch") as HTMLElement;
         wrong.style.display = "none";
-        console.log("Haool");
+        console.log("Richtig Antwort");
+        pointcounter++;
+        questioncounter++;
 
         
     }
@@ -196,11 +207,13 @@ console.log("update");
     //Falsche Antworten werden angezeigt
 
     function wrongAnswers(): void {
-        console.log("Falsch");
+        console.log("Falsch Antwort");
         let wrong: HTMLElement = document.getElementById("falsch") as HTMLElement;
         wrong.style.display = "block";
         let right2: HTMLElement = document.getElementById("richtig") as HTMLElement;
         right2.style.display = "none";
+        wrongquestioncounter++;
+        questioncounter++;
 
 
 }
@@ -216,7 +229,6 @@ console.log("update");
         
     });
     
-}
 
 //Punkte werden angezeigt
 
@@ -227,63 +239,62 @@ function showPoi(): void {
     wrongpoints.innerHTML = wrongquestioncounter.toString();}
     
 
+}   
+
+}
 
 
 //Spiel wird beendet
 
-function goalCounter(): void {
-    if (pointcounter == 5) {
-        endGame();
-    }
-}
 
-//function endGame(): void { 
-    let end: HTMLElement = document.getElementById("End") as HTMLElement;
-    end.style.display = "block";
-    let hidden: HTMLElement = document.getElementById("question-container") as HTMLElement;
-    hidden.style.display = "none";
-    let hidden2: HTMLElement = document.getElementById("End") as HTMLElement;
-    hidden2.style.display = "block";
-    let hidden3: HTMLElement = document.getElementById("ResultR") as HTMLElement;
-    hidden3.style.display = "none";
-    let hidden4: HTMLElement = document.getElementById("wrong") as HTMLElement;
-    hidden4.style.display = "none";
-    let hidden5: HTMLElement = document.getElementById("Themengebiet") as HTMLElement;
-    hidden5.style.display = "block";
-    let hidden6: HTMLElement = document.getElementById("question") as HTMLElement;
-    hidden6.style.display = "block";
-    let hidden7: HTMLElement = document.getElementById("A") as HTMLElement;
-    hidden7.style.display = "block";
-    let hidden8: HTMLElement = document.getElementById("B") as HTMLElement;
-    hidden8.style.display = "block";
-    let hidden9: HTMLElement = document.getElementById("C") as HTMLElement;
-    hidden9.style.display = "block";
-    let hidden10: HTMLElement = document.getElementById("D") as HTMLElement;
-    hidden10.style.display = "block";
-    let hidden11: HTMLElement = document.getElementById("End") as HTMLElement;
-    hidden11.style.display = "block";
-    let hidden12: HTMLElement = document.getElementById("ResultR") as HTMLElement;
-    hidden12.style.display = "block";
-    let hidden13: HTMLElement = document.getElementById("wrong") as HTMLElement;
-    hidden13.style.display = "block";
-    let hidden14: HTMLElement = document.getElementById("End") as HTMLElement;
-    hidden14.style.display = "block";
-    let hidden15: HTMLElement = document.getElementById("ResultR") as HTMLElement;
-    hidden15.style.display = "block";
-    let hidden16: HTMLElement = document.getElementById("wrong") as HTMLElement;
-    hidden16.style.display = "block";
-    let hidden17: HTMLElement = document.getElementById("End") as HTMLElement;
-    hidden17.style.display = "block";
-    let hidden18: HTMLElement = document.getElementById("ResultR") as HTMLElement;
-    hidden18.style.display = "block";
-    let hidden19: HTMLElement = document.getElementById("wrong") as HTMLElement;
-    hidden19.style.display = "block";
-    let hidden20: HTMLElement = document.getElementById("End") as HTMLElement;
+ //function endGame(): void { 
+    // if (pointcounter == 5) {
+    // let end: HTMLElement = document.getElementById("End") as HTMLElement;
+    // end.style.display = "block";
+    // let hidden: HTMLElement = document.getElementById("question-container") as HTMLElement;
+    // hidden.style.display = "none";
+    // let hidden2: HTMLElement = document.getElementById("End") as HTMLElement;
+    // hidden2.style.display = "block";
+    // let hidden3: HTMLElement = document.getElementById("ResultR") as HTMLElement;
+    // hidden3.style.display = "none";
+    // let hidden4: HTMLElement = document.getElementById("wrong") as HTMLElement;
+    // hidden4.style.display = "none";
+    // let hidden5: HTMLElement = document.getElementById("Themengebiet") as HTMLElement;
+    // hidden5.style.display = "block";
+    // let hidden6: HTMLElement = document.getElementById("question") as HTMLElement;
+    // hidden6.style.display = "block";
+    // let hidden7: HTMLElement = document.getElementById("A") as HTMLElement;
+    // hidden7.style.display = "block";
+    // let hidden8: HTMLElement = document.getElementById("B") as HTMLElement;
+    // hidden8.style.display = "block";
+    // let hidden9: HTMLElement = document.getElementById("C") as HTMLElement;
+    // hidden9.style.display = "block";
+    // let hidden10: HTMLElement = document.getElementById("D") as HTMLElement;
+    // hidden10.style.display = "block";
+    // let hidden11: HTMLElement = document.getElementById("End") as HTMLElement;
+    // hidden11.style.display = "block";
+    // let hidden12: HTMLElement = document.getElementById("ResultR") as HTMLElement;
+    // hidden12.style.display = "block";
+    // let hidden13: HTMLElement = document.getElementById("wrong") as HTMLElement;
+    // hidden13.style.display = "block";
+    // let hidden14: HTMLElement = document.getElementById("End") as HTMLElement;
+    // hidden14.style.display = "block";
+    // let hidden15: HTMLElement = document.getElementById("ResultR") as HTMLElement;
+    // hidden15.style.display = "block";
+    // let hidden16: HTMLElement = document.getElementById("wrong") as HTMLElement;
+    // hidden16.style.display = "block";
+    // let hidden17: HTMLElement = document.getElementById("End") as HTMLElement;
+    // hidden17.style.display = "block";
+    // let hidden18: HTMLElement = document.getElementById("ResultR") as HTMLElement;
+    // hidden18.style.display = "block";
+    // let hidden19: HTMLElement = document.getElementById("wrong") as HTMLElement;
+    // hidden19.style.display = "block";
+    // let hidden20: HTMLElement = document.getElementById("End") as HTMLElement;
 
 
 
 
-}
+
 
     //Für die Antworten nochmal ein shuffle, und eine for schleife, die auf die Buttons zugreift
 
